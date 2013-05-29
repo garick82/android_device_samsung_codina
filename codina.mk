28009e6d389d2aad4f672194bd171faa1d44a39d
Shaaan <shantanulondhe26@gmail.com>
2013-05-29 06:05:30 +0100

    Add configs

--- codina.mk
+++ codina.mk
@@ -7,13 +7,75 @@ DEVICE_PACKAGE_OVERLAYS := $(LOCAL_PATH)/overlay
 PRODUCT_AAPT_CONFIG := normal hdpi
 PRODUCT_AAPT_PREF_CONFIG := hdpi
 
-PRODUCT_TAGS += dalvik.gc.type-precise
+# Init Files
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilt/kernel:kernel \
+    $(LOCAL_PATH)/prebuilt/root/default.prop:root/default.prop \
+    $(LOCAL_PATH)/prebuilt/root/init.samsungcodina.rc:root/init.samsungcodina.rc \
+    $(LOCAL_PATH)/prebuilt/root/init.samsungcodina.usb.rc:root/init.samsungcodina.usb.rc \
+    $(LOCAL_PATH)/prebuilt/root/ueventd.samsungcodina.rc:root/ueventd.samsungcodina.rc \
+    $(LOCAL_PATH)/prebuilt/root/fstab.samsungcodina:root/fstab.samsungcodina \
+    $(LOCAL_PATH)/prebuilt/root/lib/modules/j4fs.ko:root/lib/modules/j4fs.ko \
+    $(LOCAL_PATH)/prebuilt/root/lib/modules/param.ko:root/lib/modules/param.ko
+
+# STE
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/omxloaders:system/omxloaders \
+    $(LOCAL_PATH)/configs/cspsa.conf:system/etc/cspsa.conf \
+    $(LOCAL_PATH)/configs/ste_modem.sh:system/etc/ste_modem.sh \
+    $(LOCAL_PATH)/configs/usbid_init.sh:system/bin/usbid_init.sh
+
+# Audio
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/asound.conf:system/etc/asound.conf \
+    $(LOCAL_PATH)/configs/audio_policy.conf:system/vendor/etc/audio_policy.conf \
+    $(LOCAL_PATH)/configs/adm.sqlite-u8500:system/etc/adm.sqlite-u8500 \
+    $(LOCAL_PATH)/configs/Volume.db:system/etc/Volume.db
+
+# Vold and Storage
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/vold.fstab:system/etc/vold.fstab
+
+# Gps
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/gps.conf:system/etc/gps.conf
+
+$(call inherit-product, device/common/gps/gps_eu_supl.mk)
+
+# Wifi
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/bcmdhd.cal:system/etc/wifi/bcmdhd.cal \
+    $(LOCAL_PATH)/configs/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    wifi.interface=wlan0 \
+    wifi.supplicant_scan_interval=15
+
+$(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/firmware/bcm4330/device-bcm.mk)
+
+# Charger
+PRODUCT_PACKAGES += \
+    charger \
+    charger_res_images
 
 # Packages
 PRODUCT_PACKAGES += \
     audio.a2dp.default \
     lights.montblanc
 
+# RIL
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.telephony.ril_class=Smdk4210RIL \
+    mobiledata.interfaces=pdp0,wlan0,gprs,ppp0 \
+    ro.ril.hsxpa=1 \
+    ro.ril.gprsclass=10
+
+# OMX
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilt/system/etc/media_codecs.xml:system/etc/media_codecs.xml \
+    $(LOCAL_PATH)/prebuilt/system/etc/media_profiles.xml:system/etc/media_profiles.xml
+
+# Hardware
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
@@ -34,49 +96,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/android.software.sip.xml:system/etc/permissions/android.software.sip.xml
 
-# Ramdisk
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/prebuilt/kernel:kernel \
-    $(LOCAL_PATH)/prebuilt/root/default.prop:root/default.prop \
-    $(LOCAL_PATH)/prebuilt/root/init.samsungcodina.rc:root/init.samsungcodina.rc \
-    $(LOCAL_PATH)/prebuilt/root/init.samsungcodina.usb.rc:root/init.samsungcodina.usb.rc \
-    $(LOCAL_PATH)/prebuilt/root/ueventd.samsungcodina.rc:root/ueventd.samsungcodina.rc \
-    $(LOCAL_PATH)/prebuilt/root/fstab.samsungcodina:root/fstab.samsungcodina \
-    $(LOCAL_PATH)/prebuilt/root/lib/modules/j4fs.ko:root/lib/modules/j4fs.ko \
-    $(LOCAL_PATH)/prebuilt/root/lib/modules/param.ko:root/lib/modules/param.ko
-
-# Display
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/prebuilt/system/lib/hw/hwcomposer.montblanc.so:system/lib/hw/hwcomposer.montblanc.so \
-    $(LOCAL_PATH)/prebuilt/system/lib/hw/gralloc.montblanc.so:system/lib/hw/gralloc.montblanc.so	
-
-# Mali-400
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/prebuilt/system/lib/egl/libEGL_mali.so:system/lib/egl/libEGL_mali.so \
-    $(LOCAL_PATH)/prebuilt/system/lib/egl/libGLESv1_CM_mali.so:system/lib/egl/libGLESv1_CM_mali.so \
-    $(LOCAL_PATH)/prebuilt/system/lib/egl/libGLESv2_mali.so:system/lib/egl/libGLESv2_mali.so \
-    $(LOCAL_PATH)/prebuilt/system/lib/libMali.so:system/lib/libMali.so \
-    $(LOCAL_PATH)/prebuilt/system/lib/libUMP.so:system/lib/libUMP.so
-
-# RIL
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.telephony.ril_class=Smdk4210RIL \
-    mobiledata.interfaces=pdp0,wlan0,gprs,ppp0 \
-    ro.ril.hsxpa=1 \
-    ro.ril.gprsclass=10
-
-# Wifi
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    wifi.interface=wlan0 \
-    wifi.supplicant_scan_interval=15
-
-# OMX
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/prebuilt/system/etc/vold.fstab:system/etc/vold.fstab \
-    $(LOCAL_PATH)/prebuilt/system/etc/media_codecs.xml:system/etc/media_codecs.xml \
-    $(LOCAL_PATH)/prebuilt/system/etc/media_profiles.xml:system/etc/media_profiles.xml
+PRODUCT_TAGS += dalvik.gc.type-precise
 
 $(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
